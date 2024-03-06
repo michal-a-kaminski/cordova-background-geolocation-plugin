ea3a02105fdb6576489b0eb88e3ddbbfca8fdacc
m.kaminski <m.kaminski@wasko.pl>
2024-03-06 06:51:03 +0100

    poprawka dla ios

--- ios/common/BackgroundGeolocation/MAURLocationManager.m
+++ ios/common/BackgroundGeolocation/MAURLocationManager.m
@@ -4,64 +4,65 @@
 //  Created by Jinru on 12/19/09.
 //  Copyright 2009 Arizona State University. All rights reserved.
 //
-
+ 
 #import "MAURLocation.h"
 #import "MAURLocationManager.h"
-
+ 
 #define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
 #define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
 #define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
 #define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
 #define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
-
+ 
 #define LOCATION_DENIED         "User denied use of location services."
 #define LOCATION_RESTRICTED     "Application's use of location services is restricted."
 #define LOCATION_NOT_DETERMINED "User undecided on application's use of location services."
-
+ 
 static MAURLocationManager* sharedCLDelegate = nil;
 static NSString *const TAG = @"MAURLocationManager";
 static NSString *const Domain = @"com.marianhello";
-
+NSTimer *timer;
+ 
 @implementation MAURLocationManager
 @synthesize locationManager, delegate;
-
+ 
 - (id)init
 {
     self = [super init];
     if (self != nil) {
         locationManager = [[CLLocationManager alloc] init];
-
+ 
         if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"9.0")) {
             locationManager.allowsBackgroundLocationUpdates = YES;
         }
-
+ 
         locationManager.delegate = self;
         locationManager.desiredAccuracy = kCLLocationAccuracyBest;
     }
     return self;
 }
-
+ 
 - (BOOL) start:(NSError * __autoreleasing *)outError
 {
     NSAssert([NSThread isMainThread], @"%@ %@", TAG, @"should only be called from the main thread.");
-
+ 
     NSUInteger authStatus;
-    
+   
     if ([CLLocationManager respondsToSelector:@selector(authorizationStatus)]) { // iOS 4.2+
         authStatus = [CLLocationManager authorizationStatus];
-        
+       
         if (authStatus == kCLAuthorizationStatusDenied) {
             if (outError != NULL) {
                 NSDictionary *errorDictionary = @{
                                                   NSLocalizedDescriptionKey: NSLocalizedString(@LOCATION_DENIED, nil)
                                                   };
-
+ 
                 *outError = [NSError errorWithDomain:Domain code:MAURBGPermissionDenied userInfo:errorDictionary];
             }
-            
+           
             return NO;
         }
-        
+       
         if (authStatus == kCLAuthorizationStatusRestricted) {
             if (outError != NULL) {
                 NSDictionary *errorDictionary = @{
@@ -69,15 +70,15 @@ static NSString *const Domain = @"com.marianhello";
                                                   };
                 *outError = [NSError errorWithDomain:Domain code:MAURBGPermissionDenied userInfo:errorDictionary];
             }
-            
+           
             return NO;
         }
-        
+       
 #ifdef __IPHONE_8_0
         // we do startUpdatingLocation even though we might not get permissions granted
         // we can stop later on when recieved callback on user denial
         // it's neccessary to start call startUpdatingLocation in iOS < 8.0 to show user prompt!
-        
+       
         if (authStatus == kCLAuthorizationStatusNotDetermined) {
             if ([locationManager respondsToSelector:@selector(requestAlwaysAuthorization)]) {  //iOS 8.0+
                 [locationManager requestAlwaysAuthorization];
@@ -85,44 +86,67 @@ static NSString *const Domain = @"com.marianhello";
         }
 #endif
     }
-    
+   
     [locationManager startUpdatingLocation];
+    if(timer){
+        [timer invalidate];
+        timer = nil;
+    }
+    NSLog(@"timer start");
+    timer = [NSTimer scheduledTimerWithTimeInterval:5.0
+    target:self
+    selector:@selector(onTick:)
+    userInfo:nil
+    repeats:YES];
     return YES;
 }
-
+ 
+-(void)onTick:(NSTimer *)timer {
+   CLLocation * lastKnownLocation  = [locationManager location];
+    CLLocation * faked = [[CLLocation alloc] initWithLatitude: lastKnownLocation.coordinate.latitude longitude:lastKnownLocation.coordinate.longitude];
+   NSArray *locations = @[faked];
+    NSLog(@"timer tick");
+   [self.delegate onLocationsChanged:locations];
+}
+ 
 - (BOOL) stop:(NSError * __autoreleasing *)outError
 {
     NSAssert([NSThread isMainThread], @"%@ %@", TAG, @"should only be called from the main thread.");
     [locationManager stopUpdatingLocation];
+    if(timer){
+        [timer invalidate];
+        timer = nil;
+    }
+    NSLog(@"timer stop");
     return YES;
 }
-
+ 
 - (BOOL) startMonitoringSignificantLocationChanges
 {
     NSAssert([NSThread isMainThread], @"%@ %@", TAG, @"should only be called from the main thread.");
     [locationManager startMonitoringSignificantLocationChanges];
     return YES;
 }
-
+ 
 - (BOOL) stopMonitoringSignificantLocationChanges
 {
     NSAssert([NSThread isMainThread], @"%@ %@", TAG, @"should only be called from the main thread.");
     [locationManager stopMonitoringSignificantLocationChanges];
     return YES;
 }
-
+ 
 - (void) startMonitoringForRegion:(CLRegion*)region
 {
     NSAssert([NSThread isMainThread], @"%@ %@", TAG, @"should only be called from the main thread.");
     [locationManager startMonitoringForRegion:region];
 }
-
+ 
 - (void) stopMonitoringForRegion:(CLRegion*)region
 {
     NSAssert([NSThread isMainThread], @"%@ %@", TAG, @"should only be called from the main thread.");
     [locationManager stopMonitoringForRegion:region];
 }
-
+ 
 - (void) stopMonitoringForRegionIdentifier:(NSString*)identifier
 {
     NSAssert([NSThread isMainThread], @"%@ %@", TAG, @"should only be called from the main thread.");
@@ -132,7 +156,7 @@ static NSString *const Domain = @"com.marianhello";
         }
     }
 }
-
+ 
 - (void) stopMonitoringAllRegions
 {
     NSAssert([NSThread isMainThread], @"%@ %@", TAG, @"should only be called from the main thread.");
@@ -140,67 +164,67 @@ static NSString *const Domain = @"com.marianhello";
         [locationManager stopMonitoringForRegion:region];
     }
 }
-
+ 
 - (NSSet<__kindof CLRegion *>*) monitoredRegions
 {
     return locationManager.monitoredRegions;
 }
-
+ 
 - (void) setShowsBackgroundLocationIndicator:(BOOL)shows
 {
     if (@available(iOS 11, *)) {
         locationManager.showsBackgroundLocationIndicator = shows;
     }
 }
-
+ 
 - (void) setPausesLocationUpdatesAutomatically:(BOOL)newPausesLocationsUpdatesAutomatically
 {
     locationManager.pausesLocationUpdatesAutomatically = newPausesLocationsUpdatesAutomatically;
 }
-
+ 
 - (BOOL) pausesLocationUpdatesAutomatically
 {
     return locationManager.pausesLocationUpdatesAutomatically;
 }
-
+ 
 - (void) setDistanceFilter:(CLLocationDistance)newDistanceFiler
 {
     locationManager.distanceFilter = newDistanceFiler;
 }
-
+ 
 - (CLLocationDistance) distanceFilter
 {
     return locationManager.distanceFilter;
 }
-
+ 
 - (void) setActivityType:(CLActivityType)newActivityType
 {
     locationManager.activityType = newActivityType;
 }
-
+ 
 - (CLActivityType) activityType
 {
     return locationManager.activityType;
 }
-
+ 
 - (void) setDesiredAccuracy:(CLLocationAccuracy)newDesiredAccuracy
 {
     locationManager.desiredAccuracy = newDesiredAccuracy;
 }
-
+ 
 - (CLLocationAccuracy) desiredAccuracy
 {
     return locationManager.desiredAccuracy;
 }
-
-
+ 
+ 
 #pragma mark -
 #pragma mark CLLocationManagerDelegate Methods
 - (void) locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
 {
     [self.delegate onLocationsChanged:locations];
 }
-
+ 
 - (void) locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
 {
     if (self.delegate && [self.delegate respondsToSelector:@selector(onError:)]) {
@@ -208,15 +232,15 @@ static NSString *const Domain = @"com.marianhello";
                                           NSUnderlyingErrorKey : error
                                           };
         NSError *outError = [NSError errorWithDomain:Domain code:MAURBGServiceError userInfo:errorDictionary];
-
+ 
         [self.delegate onError:outError];
     }
 }
-
+ 
 - (void) locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status
 {
     MAURLocationAuthorizationStatus authStatus;
-    
+   
     switch(status) {
         case kCLAuthorizationStatusRestricted:
         case kCLAuthorizationStatusDenied:
@@ -231,33 +255,33 @@ static NSString *const Domain = @"com.marianhello";
         default:
             return;
     }
-
+ 
     if (self.delegate && [self.delegate respondsToSelector:@selector(onAuthorizationChanged:)]) {
         [self.delegate onAuthorizationChanged:authStatus];
     }
 }
-
+ 
 - (void) locationManagerDidPauseLocationUpdates:(CLLocationManager *)manager
 {
     if (self.delegate && [self.delegate respondsToSelector:@selector(onLocationPause:)]) {
         [self.delegate onLocationPause:manager];
     }
 }
-
+ 
 - (void) locationManagerDidResumeLocationUpdates:(CLLocationManager *)manager
 {
     if (self.delegate && [self.delegate respondsToSelector:@selector(onLocationResume:)]) {
         [self.delegate onLocationResume:manager];
     }
 }
-
+ 
 - (void) locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region
 {
     if (self.delegate && [self.delegate respondsToSelector:@selector(onRegionExit:)]) {
         [self.delegate onRegionExit:region];
     }
 }
-
+ 
 #pragma mark - Singleton implementation in ARC
 + (MAURLocationManager *)sharedInstance
 {
@@ -268,7 +292,7 @@ static NSString *const Domain = @"com.marianhello";
     });
     return sharedLocationControllerInstance;
 }
-
+ 
 + (id)allocWithZone:(NSZone *)zone {
     @synchronized(self) {
         if (sharedCLDelegate == nil) {
@@ -278,10 +302,10 @@ static NSString *const Domain = @"com.marianhello";
     }
     return nil; // on subsequent allocation attempts return nil
 }
-
+ 
 - (id)copyWithZone:(NSZone *)zone
 {
     return self;
 }
-
+ 
 @end
